d4cb008c06d12106e3f5b15afd57e2596fc8d6cf
sebbarry <59349192+sebbarry@users.noreply.github.com>
2020-07-12 19:45:10 -0400

    Commit Beta (8). Bug fix with the input price screen to only allow one period and no other characters than numbers.

--- FizzyPoints/Controller/Employee View Controllers/PaymentScreen.swift
+++ FizzyPoints/Controller/Employee View Controllers/PaymentScreen.swift
@@ -28,12 +28,44 @@ class PaymentScreen: UIViewController, UITextFieldDelegate {
         doneButton.frame.size.height = 50
     }
     
+    //Textfield delegates
+   
     
     @IBAction func calculatePoints(_ sender: UIButton) {
         if paymentAmount.text?.isEmpty == false {
-            let inputValue = Double(paymentAmount.text!)
-            GlobalFunctions.incrementPointsButton(nameofUser: GlobalVariables.ActualIDs.ActualCustomer, nameofBusiness: GlobalVariables.ActualIDs.CurrentNameofBusiness, incrementPoints: inputValue)
-            backTwo()
+            
+            var periodCounter = 0
+            let valuesInText = paymentAmount.text!
+            var charactersArray : [String] = []
+            for characters in valuesInText {
+                charactersArray.append(String(describing: characters))
+            }
+            
+            for values in charactersArray {
+                switch values {
+                case "1", "2", "3", "4", "5", "6", "7", "8", "9", "0":
+                    print(values)
+                    break
+                case ".":
+                    periodCounter += 1
+                default:
+                    self.paymentAmount.text = ""
+                    self.paymentAmount.placeholder = "Oops, valid # please."
+                    break
+                }
+                
+                
+            }
+            
+            if periodCounter > 0 && periodCounter <= 1 {
+                let inputValue = Double(paymentAmount.text!)
+                GlobalFunctions.incrementPointsButton(nameofUser: GlobalVariables.ActualIDs.ActualCustomer, nameofBusiness: GlobalVariables.ActualIDs.CurrentNameofBusiness, incrementPoints: inputValue)
+                backTwo()
+            } else if periodCounter > 1 {
+                self.paymentAmount.text = ""
+                self.paymentAmount.placeholder = "One too many ."
+            }
+                   
             
             
             //increment the total amount the user has spent at this business over his hlifetime.
